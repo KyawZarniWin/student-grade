2b7295714ab7ea4dd715225c02b7c27139ffd549
KyawZarniWin <155807373+KyawZarniWin@users.noreply.github.com>
2026-09-14 15:03:13 +0630

    Update grade.py

--- grade.py
+++ grade.py
@@ -2,11 +2,8 @@ name = input("Enter student name: ")
 mark1 = float(input("Enter first subject mark: "))
 mark2 = float(input("Enter second subject mark: "))
 
-total = mark1 + mark2
-average = total / 2
-
+average = (mark1 + mark2) / 2
 print(f"\nStudent Name: {name}")
-print(f"Total Mark: {total}")
 print(f"Average Mark: {average}")
 
 if average >= 50:
