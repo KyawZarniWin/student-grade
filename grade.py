5033e44365ced2d2aad39d4d63a07b3fc25c765c
KyawZarniWin <kyawzarniwin.j@gmail.com>
2026-09-14 14:35:57 +0630

    Local change for total mark

--- grade.py
+++ grade.py
@@ -3,13 +3,6 @@ mark1 = float(input("Enter first subject mark: "))
 mark2 = float(input("Enter second subject mark: "))
 
 total = mark1 + mark2
-average = total / 2
-
 print(f"\nStudent Name: {name}")
 print(f"Total Mark: {total}")
-print(f"Average Mark: {average}")
 
-if average >= 50:
-    print("Result: Pass")
-else:
-    print("Result: Fail")
